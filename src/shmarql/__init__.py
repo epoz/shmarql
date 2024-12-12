233372a2cad31abfc6614790544adf242604ec57
Etienne Posthumus <eposthumus@gmail.com>
2024-12-12 15:08:28 +0100

    Add markdown plugin

--- src/shmarql/__init__.py
+++ src/shmarql/__init__.py
@@ -1,1 +1,2 @@
 from .main import app
+from .markdownplugin import makeExtension
---
+++ src/shmarql/markdownplugin.py
@@ -0,0 +1,38 @@
+from pygments.lexers.rdf import SparqlLexer
+from markdown.extensions.codehilite import CodeHiliteExtension
+from markdown import Markdown
+from pygments.lexers import find_lexer_class_by_name
+from pygments.util import ClassNotFound
+
+
+__all__ = ["ShmarqlLexer"]
+
+
+class ShmarqlLexer(SparqlLexer):
+    """A custom lexer for SPARQL syntax, mirroring the default SparqlLexer."""
+
+    name = "shmarql"
+    aliases = ["shmarql"]
+
+
+class CustomCodeBlockExtension(CodeHiliteExtension):
+    """Extend the CodeHilite extension to recognize `shmarql` blocks."""
+
+    def extendMarkdown(self, md: Markdown):
+        super().extendMarkdown(md)
+        try:
+            _ = find_lexer_class_by_name("shmarql")
+        except ClassNotFound:
+            from pygments.lexers import LEXERS
+
+            LEXERS["ShmarqlLexer"] = (
+                "shmarql.markdownplugin",
+                ShmarqlLexer.name,
+                tuple(ShmarqlLexer.aliases),
+                ShmarqlLexer.filenames,
+                ShmarqlLexer.mimetypes,
+            )
+
+
+def makeExtension(**kwargs):
+    return CustomCodeBlockExtension(lang="shmarql")
